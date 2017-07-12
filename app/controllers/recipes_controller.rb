f1e3ac7c3f2632ccfd8e7dbc37c5810b68206428
Rails Udemy <abhigeek8@gmail.com>
2017-07-13 01:09:41 +0530

    Add new recipe creation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,36 @@
 class RecipesController < ApplicationController
 
+  before_action :set_recipe, only: [:show, :edit, :destroy, :update]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
+
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = "Recipe was successfully created"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
+
+  def set_recipe
     @recipe = Recipe.find(params[:id])
   end
 end
