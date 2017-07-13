fa4b09c522f6b39226a3464051730c9ae1c9195c
Rails Udemy <abhigeek8@gmail.com>
2017-07-13 14:30:40 +0530

    Implemented edit and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 
-  before_action :set_recipe, only: [:show, :edit, :destroy, :update]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   def index
     @recipes = Recipe.all
@@ -23,6 +23,25 @@ class RecipesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was successfully updated"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    flash[:danger] = "Recipe was successfully deleted"
+    redirect_to recipes_path
   end
 
   private
